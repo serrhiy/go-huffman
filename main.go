e85bd8dd2a218698f7ee77aa23264191473420c1
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-12-06 01:49:29 +0200

    Parse arguments

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
+const OutputExtension = ".hfm"
+
+var input = flag.String("input", "", "path to the input file")
+var output = flag.String("output", "", "path to the output file")
+
+func getArguments() (string, string, error) {
+	if *input == "" {
+		return "", "", errors.New("input argument is mandatory")
+	}
+
+	if *output == "" {
+		base := filepath.Base(*input)
+		ext := filepath.Ext(base)
+		name := base[:len(base)-len(ext)]
+		return *input, fmt.Sprintf("%s%s", name, OutputExtension), nil
+	}
+
+	return *input, *output, nil
+}
+
 func main() {
-	fmt.Println("Helllo world!")
+	flag.Parse()
+
+	inputFile, outputFile, err := getArguments()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error occurred: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Passed args are %s, %s\n", inputFile, outputFile)
 }

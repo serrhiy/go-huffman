dd6b55d0da08740ca9130a885d1a6aef1cb7d2b8
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-12-14 13:21:49 +0200

    Update tests for huffman.toPriorityQueue

--- huffman/utils_test.go
+++ huffman/utils_test.go
@@ -33,60 +33,61 @@ func validateInternalCounts(t *testing.T, n *node) uint {
 	return n.count
 }
 
-func TestToPriorityQueueEmpty(t *testing.T) {
-	freq := map[byte]uint{}
-	pq := toPriorityQueue(freq)
+func TestToPriorityQueue(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		pq := toPriorityQueue(map[byte]uint{})
 
-	if pq.Len() != 0 {
-		t.Fatalf("expected empty priority queue, got %d", pq.Len())
-	}
-}
-
-func TestToPriorityQueueSingleElement(t *testing.T) {
-	freq := map[byte]uint{'A': 5}
-	pq := toPriorityQueue(freq)
-
-	if pq.Len() != 1 {
-		t.Fatalf("expected queue length 1, got %d", pq.Len())
-	}
-
-	item, ok := heap.Pop(&pq).(*node)
-	if !ok || item.char != 'A' || item.count != 5 {
-		t.Fatalf("unexpected element: got {%c, %d}", item.char, item.count)
-	}
-}
-
-func TestToPriorityQueueMultiple(t *testing.T) {
-	freq := map[byte]uint{
-		'A': 5,
-		'B': 2,
-		'C': 9,
-		'D': 1,
-	}
-
-	pq := toPriorityQueue(freq)
-
-	if pq.Len() != 4 {
-		t.Fatalf("expected length 4, got %d", pq.Len())
-	}
-
-	expectedOrder := []struct {
-		char  byte
-		count uint
-	}{
-		{'D', 1},
-		{'B', 2},
-		{'A', 5},
-		{'C', 9},
-	}
-
-	for i, exp := range expectedOrder {
-		item, ok := heap.Pop(&pq).(*node)
-		if !ok || item.char != exp.char || item.count != exp.count {
-			t.Fatalf("at pop %d: expected {%c, %d}, got {%c, %d}",
-				i, exp.char, exp.count, item.char, item.count)
+		if pq.Len() != 0 {
+			t.Fatalf("expected empty priority queue, got %d", pq.Len())
 		}
-	}
+	})
+
+	t.Run("single element", func(t *testing.T) {
+		freq := map[byte]uint{'a': 5}
+		pq := toPriorityQueue(freq)
+
+		if pq.Len() != 1 {
+			t.Fatalf("expected queue length 1, got %d", pq.Len())
+		}
+
+		item, ok := heap.Pop(&pq).(*node)
+		if !ok || item.char != 'a' || item.count != 5 {
+			t.Fatalf("unexpected element: got {%c, %d}", item.char, item.count)
+		}
+	})
+
+	t.Run("multiple items", func(t *testing.T) {
+		freq := map[byte]uint{
+			'a': 5,
+			'b': 2,
+			'c': 9,
+			'd': 1,
+		}
+
+		pq := toPriorityQueue(freq)
+
+		if pq.Len() != 4 {
+			t.Fatalf("expected length 4, got %d", pq.Len())
+		}
+
+		expectedOrder := []struct {
+			char  byte
+			count uint
+		}{
+			{'d', 1},
+			{'b', 2},
+			{'a', 5},
+			{'c', 9},
+		}
+
+		for i, exp := range expectedOrder {
+			item, ok := heap.Pop(&pq).(*node)
+			if !ok || item.char != exp.char || item.count != exp.count {
+				t.Fatalf("at pop %d: expected {%c, %d}, got {%c, %d}",
+					i, exp.char, exp.count, item.char, item.count)
+			}
+		}
+	})
 }
 
 func TestBuildTreeSingleNode(t *testing.T) {
